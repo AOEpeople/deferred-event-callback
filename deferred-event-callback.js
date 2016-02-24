8d275b1e58a3793c13a672d18be2ea45dce5b98c
Marc Dix <marc.dix@aoe.com>
2016-02-24 13:32:45 +0100

    Adding possibility to add events to multiple nodes

--- deferred-event-callback.js
+++ deferred-event-callback.js
@@ -2,17 +2,21 @@
 
 module.exports = function(options, callback) {
     if (!options.eventName) throw new Error('Missing eventName property in options object.');
-    if (!options.node) throw new Error('Missing node to add event listener.');
-    if (typeof callback !== 'function') throw new Error('Missing callback function.');;
+    if (options.nodes instanceof Array !== true || options.nodes.length < 1) throw new Error('Missing nodes array with elements to add event listeners.');
+    if (typeof callback !== 'function') throw new Error('Missing callback function.');
 
     if (window.attachEvent) {
-        options.node.attachEvent('on' + options.eventName, function() {
-            executeCallbackDeferred();
+        options.nodes.forEach(function(node) {
+            node.attachEvent('on' + options.eventName, function() {
+                executeCallbackDeferred();
+            });
         });
     } else if (window.addEventListener) {
-        options.node.addEventListener(options.eventName, function() {
-            executeCallbackDeferred();
-        }, true);
+        options.nodes.forEach(function(node) {
+            node.addEventListener(options.eventName, function() {
+                executeCallbackDeferred();
+            }, true);
+        });
     } else {
         throw new Error('Neither attachEvent nor addEventListener found. Are you using a browser or a dishwasher?');
     }
