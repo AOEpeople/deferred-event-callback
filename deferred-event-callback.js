60127b79c9be2061ad6bf5baa09f6457af2ad7e7
Marc Dix <marc.dix@aoe.com>
2016-02-24 14:51:33 +0100

    Adding possibility to pass multiple event names

--- deferred-event-callback.js
+++ deferred-event-callback.js
@@ -1,30 +1,35 @@
 "use strict";
 
 module.exports = function(options, callback) {
-    if (!options.eventName) throw new Error('Missing eventName property in options object.');
+    if (options.eventNames instanceof Array !== true) throw new Error('Missing eventNames array.');
+    if (options.eventNames.length < 1) return;
     if (options.nodes instanceof Array !== true) throw new Error('Missing nodes array with elements to add event listeners.');
     if (options.nodes.length < 1) return;
     if (typeof callback !== 'function') throw new Error('Missing callback function.');
 
     if (window.attachEvent) {
         options.nodes.forEach(function(node) {
-            node.attachEvent('on' + options.eventName, function() {
-                executeCallbackDeferred();
+            options.eventNames.forEach(function(eventName) {
+                node.attachEvent('on' + eventName, function() {
+                    executeCallbackDeferred(node);
+                });
             });
         });
     } else if (window.addEventListener) {
         options.nodes.forEach(function(node) {
-            node.addEventListener(options.eventName, function() {
-                executeCallbackDeferred();
-            }, true);
+            options.eventNames.forEach(function(eventName) {
+                node.addEventListener(eventName, function() {
+                    executeCallbackDeferred(node);
+                }, true);
+            });
         });
     } else {
         throw new Error('Neither attachEvent nor addEventListener found. Are you using a browser or a dishwasher?');
     }
 
     var id;
-    function executeCallbackDeferred() {
+    function executeCallbackDeferred(node) {
         clearTimeout(id);
-        id = setTimeout(callback, options.timeoutValue || 300);
+        id = setTimeout(function() { callback(node) }, options.timeoutValue || 300);
     }
 };
